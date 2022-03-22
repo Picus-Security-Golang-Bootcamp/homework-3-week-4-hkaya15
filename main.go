e336dca6be36a82ad93719007bce185007bbf2e2
hkaya15 <kayahuseyin15@gmail.com>
2022-03-22 23:33:44 +0300

    Add DB connection

--- main.go
+++ main.go
@@ -4,14 +4,29 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 
+	"github.com/hkaya15/PicusSecurity/Week_4_Homework/base/db"
 	. "github.com/hkaya15/PicusSecurity/Week_4_Homework/book/model"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	x, e := GetAllBooks()
-	fmt.Println(x, e)
+	//x, e := GetAllBooks()
+	//fmt.Println(x, e)
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	//fmt.Println(os.Getenv("PICUS_DB_NAME"))
+	d, err := db.CreatePostgreSQL()
+	if err != nil {
+		log.Fatal("DB cannot init")
+	}
+	log.Println("Postgres connected: ", d)
 }
 func GetAllBooks() (*Books, error) {
 
