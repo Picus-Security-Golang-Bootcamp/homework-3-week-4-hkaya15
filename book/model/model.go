d4de49b3494d3850c0e714d025c5802a182b2eaa
hkaya15 <kayahuseyin15@gmail.com>
2022-03-25 22:30:57 +0300

    Update book model

--- book/model/model.go
+++ book/model/model.go
@@ -15,7 +15,7 @@ type Books struct {
 type Book struct {
 	gorm.Model
 	BookID     int     `json:"BookID" gorm:"not_null"`
-	BookName       string  `json:"Name"`
+	BookName   string  `json:"Name"`
 	Page       int     `json:"Page"`
 	StockCount uint    `json:"StockCount"`
 	Price      float64 `json:"Price" gorm:"type:numeric(10,2)"`
@@ -24,10 +24,6 @@ type Book struct {
 	AuthorID   int     `json:"AuthorID"`
 	Authors    Author  `gorm:"foreignKey:ID; references:AuthorID"`
 }
-
-// func (Book) TableName() string {
-// 	return "Book"
-// }
 
 func (b *Book) ToString() string {
 	return fmt.Sprintf("ID : %d, Book Name : %s, Page : %v, StockCount : %v,Price : %v, StockID : %v, ISBN : %v, AuthorID: %v, Author Name: %v,CreatedAt : %s", b.BookID, b.BookName, b.Page, b.StockCount, b.Price, b.StockID, b.ISBN, b.AuthorID, b.Authors.Name, b.CreatedAt.Format("2006-01-02 15:04:05"))
@@ -42,4 +38,3 @@ func (b *Book) AfterDelete(tx *gorm.DB) (err error) {
 	fmt.Printf("Book (%s) deleted!\n", b.BookName)
 	return nil
 }
-
